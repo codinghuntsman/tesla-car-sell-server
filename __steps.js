9cd3ef20d410660d9092312ff3669db17dee45e4
Sajedul-Islam <sajedulislam.cse@gmail.com>
2022-05-29 09:37:49 +0600

    steps added

--- __steps.js
+++ __steps.js
@@ -19,4 +19,25 @@
  * 
  **-------Use this command server side for Run-------------
  * npm run start-dev
+ * 
+ * 
+ * ----------------------------------------------
+ **Heroku deploy one time for your computer
+ * --------------------------------------------
+ * 1. Create heroku account
+ * 2. Verify email
+ * 3. Install heroku CLi
+ * 4. heroku login
+ * 
+ * -----------------------------------
+ **For each project one time
+ * ----------------------------------
+ * 1. heroku create (make sure you locally ok or not)
+ * 2.make sure you: git add. , git commit -m "message", git push
+ * 3. git push heroku main
+ * 4. Go to heroku dashboard > current project > reveal config verse
+ * 5. copy paste config verse from .env file
+ * 6. after completing copy paste config verse then copy heroku apps root url and replace localhost:3000
+ * 
+ * 6.Select localhost:3000 link and press the command for all select ctrl+shift+f (command for select localhost link)
 */
